77d31d60f0977cb25d4e7d97e993349baeecbe2b
Elia Armenti <armentielia@gmail.com>
2024-02-23 21:23:02 +0100

    ultime modifiche

---
+++ prova.js
@@ -0,0 +1,46 @@
+console.log ('adieu');
+
+// dichiarazione variabile
+let firstname;
+
+//assegnazione valore
+firstname = 'armenti';
+
+console.log (firstname);
+
+//dichiarazione e assegnazione
+
+let age = 18;
+
+console.log(age);
+
+let welcome = prompt ('welcome');
+
+
+console.log(lastname);
+var lastname = 'ajhdghjgdjh';
+
+//regole per dichiarare una variabile
+/*
+    let parola
+    let $parola
+    let _parola
+    let parola_parola
+    let parola1
+*/
+
+//NN è consentito
+/*
+    non si può mettere il numero prima della parola
+    let test test
+    let #test
+    let test%
+    let test 1
+*/
+
+//COSTANTI
+const address = 'dhdhdehdhehqehe';
+
+address = 'dhbshdhjsdjhsdhjs';
+
+console.log (address);
---
+++ prova2.js
@@ -0,0 +1,28 @@
+let num1;
+
+num1 = 10;
+
+let num2;
+
+num2 = 11;
+
+console.log('il valore della variabile num1 è 10');
+
+console.log('il valore della variabile num2 è 11');
+
+let stringa;
+
+stringa = 12;
+
+console.log('il valore della variabile stringa è 13');
+
+const PIGRECO = 3.17;
+
+console.log('il valore di PIGRECO è 3.141559');
+
+
+
+
+
+
+
